3fde526428be892568ae054d9e8c2aa8a6de5a29
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-10-14 10:44:06 +0530

    AI image generator added

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,7 +1,17 @@
 export default function Login () {
+
+    const [inputtext, setInputtext] = useState("")
+	const [imageurl, setImageUrl] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [hash, setHash] = useState("");
+
+	const handleInputChange = (event) => {
+		setInputtext(event.target.value);
+	}
+    
     return (
         <div>
-            Test
+            
         </div>
     )
 }
